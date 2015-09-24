4f8a650c531eea0251763513655c4f9a5ebb3be3
AnSwErYWJ <ywj1993@yeah.net>
2015-09-24 17:37:38 +0800

    程序配置

---
+++ configure.h
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: configure.h
+	> Author: AnSwEr
+	> Mail: 1045837697@qq.com
+	> Created Time: 2015年09月24日 星期四 17时27分20秒
+ ************************************************************************/
+
+#ifndef _CONFIGURE_H
+#define _CONFIGURE_H
+
+#define TRUE 1
+#define FALSE 0
+
+#endif
