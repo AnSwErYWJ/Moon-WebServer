c8e094b9b4508834c89d390130e5fef4be9c2b57
AnSwErYWJ <ywj1993@yeah.net>
2015-09-25 19:01:25 +0800

    功能接口

--- function.c
+++ function.c
@@ -12,6 +12,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
+#include <stdlib.h>
+#include "function.h"
+#include <string.h>
 
 /*
  * safe to read and write
@@ -63,4 +66,160 @@ extern ssize_t safe_write(int fd,const void *vptr,size_t n)
     }
 
     return n;
+}
+
+/*send error message to client*/
+extern void sendError(void *client_sockfd)
+{
+    int c_sockfd = *((int *)client_sockfd);
+
+    char status[] = "HTTP/1.0 400 Bad Request\r\n";
+    char header[] = "Server: Moon Server\r\nContent-Type: text/html\r\n\r\n";
+    char body[] = "<html><head><title>Bad Request</title></head><body><p>Connect failed,please check network!</p></body></html>";
+
+    /*send*/
+    safe_write(c_sockfd,status,strlen(status));
+    safe_write(c_sockfd,header,strlen(header));
+    safe_write(c_sockfd,body,strlen(body));
+}
+
+
+/*read file and send*/
+extern void sendDate(void *client_sockfd,char *filename)
+{
+    int c_sockfd = *((int *)client_sockfd);
+    char name[20];
+    char ext[10];
+
+    strcpy(name,filename);
+
+    /*check ext*/
+    strtok(name,".");
+    strcpy(ext,strtok(NULL,"."));
+    if(strcmp(ext,"php") == 0)
+    {
+        
+    }
+    else if(strcmp(ext,"html") == 0)
+    {
+        catHTML(client_sockfd,filename);
+    }
+    else if(strcmp(ext,"jpg") == 0)
+    {
+        catJPEG(client_sockfd,filename);
+    }
+    else
+    {
+        Debug("Error:ext\n");
+        sendError(client_sockfd);
+        close(c_sockfd);
+    }
+}
+
+extern void catHTML(void *client_sockfd,char *filename)
+{
+    char buf[MAXSIZE];
+    int c_sockfd = *((int *)client_sockfd);
+    FILE *fp;
+
+    char status[] = "HTTP/1.0 200 OK\r\n";
+    char header[] = "Server: Moon Server\r\nContent-Type: text/html\r\n\r\n";
+
+    /*send status and header*/
+    safe_write(c_sockfd,status,strlen(status));
+    safe_write(c_sockfd,header,strlen(header));
+
+    /*open file*/
+    fp = fopen(filename,"r");
+    if(fp == NULL)
+    {
+        Debug("Error:fopen()-html\n");
+        sendError(client_sockfd);
+        close(c_sockfd);
+    }
+
+    /*send*/
+    fgets(buf,sizeof(buf),fp);
+    while(!feof(fp))
+    {
+        safe_write(c_sockfd,buf,strlen(buf));
+        fgets(buf,sizeof(buf),fp);
+    }
+
+    fclose(fp);
+    close(c_sockfd);
+}
+
+extern void catJPEG(void *client_sockfd,char *filename)
+{
+    char buf[MAXSIZE];
+    int c_sockfd = *((int *)client_sockfd);
+    FILE *fp;
+    FILE *fw;
+
+    char status[] = "HTTP/1.0 200 OK\r\n";
+    char header[] = "Server: Moon Server\r\nContent-Type: image/jpeg\r\n\r\n";
+
+    /*send status and header*/
+    safe_write(c_sockfd,status,strlen(status));
+    safe_write(c_sockfd,header,strlen(header));
+
+    /*open jpeg in binary*/
+    fp = fopen(filename,"rb");
+    if(fp == NULL)
+    {
+        Debug("Error:fopen()-JPEG\n");
+        sendError(client_sockfd);
+        close(c_sockfd);
+    }
+
+    /*open fp in fd*/
+    fw = fdopen(c_sockfd,"w");
+    fread(buf,1,sizeof(buf),fp);
+    while(!feof(fp))
+    {
+        fwrite(buf,1,sizeof(buf),fw);
+        fread(buf,1,sizeof(buf),fp);
+    }
+
+    fclose(fw);
+    fclose(fp);
+    close(c_sockfd);
+}
+
+/*
+ * handle request
+ */
+extern void handleRequest(void *client_sockfd) 
+{
+    char buf[MAXSIZE];
+    int c_sockfd = *((int *) client_sockfd);
+    char method[10]; //请求方法
+    char filename[20]; //请求文件名
+
+    /*read request*/
+    read(c_sockfd,buf,sizeof(buf)-1);
+
+    /*check if http*/
+    if(strstr(buf,"HTTP/") == NULL)
+    {
+        Debug("Error: Not HTTP\n");
+        sendError(client_sockfd);
+        close(c_sockfd);
+    }
+
+    /*take method and check if GET*/
+    strcpy(method,strtok(buf," /"));
+    if(strcmp(method,"GET") != 0)
+    {
+        Debug("Error:Not GET\n");
+        sendError(client_sockfd);
+        close(c_sockfd);
+    }
+
+    /*take filename*/
+    strcpy(filename,strtok(NULL," /"));
+
+    /*read file and send*/
+    sendDate(client_sockfd,filename);
 }
