ce6bc9fec9e2ba709fdd7e93c82f0da95e7433b7
AnSwErYWJ <ywj1993@yeah.net>
2015-09-25 19:05:38 +0800

    服务器主程序

--- Moon.c
+++ Moon.c
@@ -10,30 +10,12 @@
 */
 
 #include <stdio.h>
-#include "configure.h"
 #include "function.h"
 #include <sys/socket.h>
 #include <strings.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
-
-/*
-* function: Debug
-*/
-#define Debug_Flag TRUE
-
-#if defined(Debug_Flag)
-#define Debug(...)\
-        do{\
-           fprintf(stderr,"-----DEBUG-----\n");\
-           fprintf(stderr,"%s %s\n",__TIME__,__DATE__);\
-           fprintf(stderr,"%s:%d:%s():",__FILE__,__LINE__,__func__);\
-           fprintf(stderr,__VA_ARGS__);\
-          }while(0)
-#endif
-
-static int ret;
 
 /*
 * listen()的包裹函数，可以自定义backlog
@@ -76,6 +58,8 @@ int main(int argc,const char *argv[])
     /*listen*/
     Listen(ser_sockfd,5);
 
+    while(1)
+    {
         /*accept*/
         cli_addr_size = sizeof(cli_addr);
         bzero(&cli_addr,cli_addr_size);
@@ -83,23 +67,9 @@ int main(int argc,const char *argv[])
         cli_sockfd = accept(ser_sockfd,(struct sockaddr *)&cli_addr,&cli_addr_size);
         if(cli_sockfd == -1)
             Debug("Error:accept()\n");
-        
-        /*read form client*/
-        ret = read(cli_sockfd,buf,sizeof(buf)-1);
-        if(ret == -1)
-            Debug("Error:safe_read()\n");
-        printf("%s",buf);
 
-        /*write to client*/
-        ret = safe_write(cli_sockfd,status,sizeof(status));
-        if(ret == -1)
-            Debug("Error:safe_write()--status\n");
-        ret = safe_write(cli_sockfd,header,sizeof(header));
-        if(ret == -1)
-            Debug("Error:safe_write()--header\n");
-        ret = safe_write(cli_sockfd,body,sizeof(body));
-        if(ret == -1)
-            Debug("Error:safe_write()--body\n");
+        handleRequest((void *)&cli_sockfd);
+    }
 
     /*close fd*/
     close(ser_sockfd);
